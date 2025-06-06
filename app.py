5b7b1365caaf21fa26402837b39d27df8153e09c
N-SriKrishna <nurandusrikrishna@gmail.com>
2025-06-06 14:05:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,29 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 
-with open("tokenizer.pkl","rb") as file:
-    tokenizer=pickle.load(file)
-model=load_model("topic_classification.keras")
+with open("tokenizer.pkl", "rb") as file:
+    tokenizer = pickle.load(file)
 
-max_length=100
-labels = ["Chemistry", "Physics"]
+model = load_model("subject_classifier.keras")
+
+max_length = 100
+labels = ["Chemistry", "Physics", "Maths"]  
 
 st.title("Subject Classifier")
-st.write("enter a question to classify it into Physics or Chemistry")
+st.write("Enter a question to classify it into Chemistry, Physics, or Maths")
 
-user_input=st.text_area("Your Question")
+user_input = st.text_area("Your Question")
 
 if st.button("Classify"):
     if user_input.strip() == "":
-        st.warning("enter a questiion")
+        st.warning("Please enter a question")
     else:
-        seq=tokenizer.texts_to_sequences([user_input])
-        padded=pad_sequences(seq,maxlen=max_length,padding="post")
-        prediction=model.predict(padded)
-        pred_class = int(prediction[0] > 0.5)
-        st.success(f"Predicted Subject : {labels[pred_class]}")
-        st.write("Raw prediction probability:", float(prediction[0]))
+        seq = tokenizer.texts_to_sequences([user_input])
+        padded = pad_sequences(seq, maxlen=max_length, padding="post")
+        prediction = model.predict(padded)  
+        
+        pred_class = np.argmax(prediction[0])  
+        pred_prob = prediction[0][pred_class]
+        
+        st.success(f"Predicted Subject: {labels[pred_class]}")
+        st.write(f"Prediction confidence: {pred_prob:.4f}")
